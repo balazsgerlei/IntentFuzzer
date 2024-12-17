1a7aadec617cc4aa69044c4ab925f52f007d730d
Balázs Gerlei <gerlotdev@gmail.com>
2024-12-17 17:44:54 +0100

    Remove unused Settings menu and resources

--- app/src/main/java/dev/gerlot/intentfuzzer/MainActivity.kt
+++ app/src/main/java/dev/gerlot/intentfuzzer/MainActivity.kt
@@ -48,9 +48,4 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
 }
